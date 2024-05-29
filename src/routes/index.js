2daaa17e2a113d56574bb0b2dea6f1abd29a6bbf
Huy0110 <duchuy01102001@gmail.com0>
2024-05-29 23:40:46 +0700

    Add upload api

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,6 +13,7 @@ router.use('/v1/api/order', require('./order'))
 router.use('/v1/api/product', require('./product'))
 router.use('/v1/api/category', require('./category'))
 router.use('/v1/api/promoItem', require('./promoItem'))
+router.use('/v1/api/upload', require('./upload'))
 router.use('/v1/api', require('./access'))
 
 module.exports = router
